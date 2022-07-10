d76acafc56935b6ae738a0d4da622bb3d35970bb
Isabella934 <isabella.lessa27@gmail.com>
2022-07-10 16:32:25 -0300

    desafio-02 de lógica

---
+++ script.js
@@ -0,0 +1,21 @@
+if(maioridade >= 18){
+    console.log()
+}else{
+    console.log()
+}
+
+let humanidade = true
+if(maioridade >= 18 && humanidade = true){
+    console.log()
+}else{
+    console.log()
+}
+
+let mes = "janeiro";
+let aniversario = "dezembro";
+
+if(mes = "janeiro" || aniversario = "dezembro"){
+    console.log()
+}else{
+    console.log()
+}
